244b99e78849cac43831efe1cc8d866d86adc9c2
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-04-23 16:39:17 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="viktor"
+idade=16
+email='vk.g.ccvitin@gmail.com'
+telefone='17 99785-4857'
+nota=8.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
