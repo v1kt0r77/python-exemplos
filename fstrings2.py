46952aba9b09375a5a66403a840c65769c975b16
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-02 16:31:58 -0300

    criação fstrings2.py

---
+++ fstrings2.py
@@ -0,0 +1,7 @@
+#formação de strings com várias linhas
+escola = 'senai'
+curso = 'Desenvolvimento de Sistemas'
+uc = 'lógica de programação'
+print(f"escola: {escola}\n"
+      f"curso: {curso}\n"
+      f"unidade curricular: {uc}")
