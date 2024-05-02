4e36557f180c63cccd4c184c7fa9c9176750d54c
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-02 16:45:16 -0300

    atulização fstrings2.py

--- fstrings2.py
+++ fstrings2.py
@@ -2,6 +2,10 @@
 escola = 'senai'
 curso = 'Desenvolvimento de Sistemas'
 uc = 'lógica de programação'
+matricula = 34
+nota = 8.999999
 print(f"escola: {escola}\n"
       f"curso: {curso}\n"
-      f"unidade curricular: {uc}")
+      f"unidade curricular: {uc}\n"
+      f"matricula: {matricula:06d}\n"
+      f"nota: {nota:.2f}")
