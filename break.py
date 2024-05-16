0ba5e39821e32a95097c8fef113650d604573639
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-16 09:24:06 -0300

    criação break.py

---
+++ break.py
@@ -0,0 +1,10 @@
+print('Operação de Divisão')
+while True:
+    n1=int(input('Informe o primeiro número: '))
+    n2=int(Input('Informe o segundo número: '))
+    if n2 == 0:
+         print('divisor não pode ser 0')
+         break
+    print(f"{n1} / {n2} = {n1/n2}")
+    print(f"{20 * 'x'}")
+print('fim do programa')                                         
