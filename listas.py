c2ba0a3dc44a2d54fbeebb5a66476ed0fa575d5f
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-21 14:57:49 -0300

    listas.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Viktor Gabriel", 4, 8.5, True]
+print(f"Nome: {aluno[0]}")
+print(f"Falta: {aluno[1]}")
+print(f"Média: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: sim")
+else:
+    print(f"aprovado: não")
