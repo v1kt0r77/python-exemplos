2d3eb14c8ab649583d4c5a197f4a520fcfc6a154
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-23 10:47:03 -0300

    listas_contar_elemento.py

---
+++ listas_comprimentos.py
@@ -0,0 +1,4 @@
+alunos=['viktor','lucas','carlos','marcos','gustavo','guilherme','andrade','viktor']
+elementos = len(alunos)
+print(f' a lista alunos possui {elementos} elemntos.')
+
---
+++ listas_contar_elemento.py
@@ -0,0 +1,5 @@
+alunos=['viktor','lucas','carlos','marcos','gustavo','guilherme','andrade','viktor']
+
+ocorrencias = alunos.count('viktor')
+print(f"Quantidade de ocorrencias de viktor:
+{ocorrencias}")
