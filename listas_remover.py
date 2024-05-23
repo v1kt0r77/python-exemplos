bc2a7b8035590cc6eae4ecc0e69a24d18c7190df
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-23 09:30:21 -0300

    listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos=['viktor','lucas','carlos','marcos','gustavo','guilherme','andrade','viktor']
+print(f"lista de alunos: (alunos)")
+# metodo remove()
+alunos.remove('viktor')
+print(f"listas de alunos: (alunos)")
+# metodo pop()
+alunos.pop(2)
+print(f"lista de alunos: (alunos)")
+# função del
+del alunos[3]
+print(f"lista de alunos: (alunos)")
+# metodo clear()
+alunos.clear()
+print(f"lista de alunos: (alunos)") 
