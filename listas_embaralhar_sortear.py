2ae91dc3b1a69fea5f7d50b4136d8aa4129d6d7f
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-23 08:54:19 -0300

    listas_embaralhar_sortear.py

---
+++ listas_embaralhar_sortear.py
@@ -0,0 +1,9 @@
+import random
+alunos=['viktor','lucas','carlos','marcos','gustavo','guilherme']
+print(f"lista: (alunoa")
+# ebaralhar a lista
+random.shuffle(alunos)
+print(f"lista embaralhada: (alunos")
+# escolhe um aluno aleatoriamente
+aluno_sorteado = random.choice(alunos)
+print(f"aluno sorteado: (aluno_sorteado)") 
