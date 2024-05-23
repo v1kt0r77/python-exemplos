9b8c4c680b6787feb13a80986b1e11810703f0f9
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-23 09:10:28 -0300

    listas_ordenação.py

---
+++ listas_ordenação.py
@@ -0,0 +1,12 @@
+import random
+alunos=['viktor','lucas','carlos','marcos','gustavo','guilherme']
+print(f"lista: (alunoa")
+# ebaralhar a lista
+random.shuffle(alunos)
+print(f"lista embaralhada: (alunos")
+#ordena a lista cresente
+alunos.sort()
+print(f"listas crecente: (alunos)")
+#ordena a lista decrecente
+alunos.sort(reverse=True)
+print(f"lista decrecente: (alunos)")
